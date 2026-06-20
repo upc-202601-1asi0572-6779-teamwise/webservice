a580c3a75bd1745ad5ad5b776813b6eceb9eadca
VRojas1603 <rojas.vmanuel16@gmail.com>
2026-06-20 13:02:34 -0500

    feat: add SensorReadingViewResourceFromAggregateAssembler.

---
+++ SmartPalmPlatform.API/SensorDataProcessing/Interfaces/REST/Transform/SensorReadingViewResourceFromAggregateAssembler.cs
@@ -0,0 +1,26 @@
+using SmartPalmPlatform.API.SensorDataProcessing.Domain.Model.Aggregates;
+using SmartPalmPlatform.API.SensorDataProcessing.Interfaces.REST.Resources;
+
+namespace SmartPalmPlatform.API.SensorDataProcessing.Interfaces.REST.Transform;
+
+public static class SensorReadingViewResourceFromAggregateAssembler
+{
+    public static SensorReadingViewResource ToResourceFromAggregate(SensorReading reading)
+    {
+        return new SensorReadingViewResource(
+            reading.Id,
+            reading.EdgeDeviceMacAddress,
+            StringFromSensorTypeAssembler.FromSensorTypeToString(reading.Type),
+            reading.Value,
+            reading.Unit.ToString(),
+            reading.MeasuredAt
+        );
+    }
+
+    public static List<SensorReadingViewResource> ToResourceListFromAggregateList(
+        List<SensorReading> readings
+    )
+    {
+        return readings.Select(ToResourceFromAggregate).ToList();
+    }
+}
