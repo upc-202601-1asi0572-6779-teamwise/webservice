a6ce92e31f52b8d52960c94b838552c5a5cbb47d
0renzo0loli0 <u20161c993@upc.edu.pe>
2026-07-08 21:24:59 -0500

    feat: added IAM facade ACL

--- SmartPalmPlatform.API/IAM/Interfaces/ACL/IIamContextFacade.cs
+++ SmartPalmPlatform.API/IAM/Interfaces/ACL/IIamContextFacade.cs
@@ -5,4 +5,7 @@ public interface IIamContextFacade
     Task<int> CreateUser(string username, string password);
     Task<int> FetchUserIdByUsername(string username);
     Task<string> FetchUsernameByUserId(int userId);
+    Task<bool> UserExistsAsync(string username);
+    Task<bool> UserExistsByIdAsync(int userId);
+    Task<bool> HasActiveSubscriptionAsync(int userId);
 }
--- SmartPalmPlatform.API/IAM/Interfaces/ACL/Services/IamContextFacade.cs
+++ SmartPalmPlatform.API/IAM/Interfaces/ACL/Services/IamContextFacade.cs
@@ -1,10 +1,16 @@
 using SmartPalmPlatform.API.IAM.Domain.Model.Commands;
+using SmartPalmPlatform.API.IAM.Domain.Model.Enums;
 using SmartPalmPlatform.API.IAM.Domain.Model.Queries;
 using SmartPalmPlatform.API.IAM.Domain.Services;
+using SmartPalmPlatform.API.IAM.Domain.Services.QueryServices;
 
 namespace SmartPalmPlatform.API.IAM.Interfaces.ACL.Services;
 
-public class IamContextFacade(IUserCommandService userCommandService, IUserQueryService userQueryService) : IIamContextFacade
+public class IamContextFacade(
+    IUserCommandService userCommandService,
+    IUserQueryService userQueryService,
+    ISubscriptionQueryService subscriptionQueryService
+) : IIamContextFacade
 {
     public async Task<int> CreateUser(string username, string password)
     {
@@ -27,5 +33,28 @@ public class IamContextFacade(IUserCommandService userCommandService, IUserQuery
         var getUserByIdQuery = new GetUserByIdQuery(userId);
         var result = await userQueryService.Handle(getUserByIdQuery);
         return result?.Username ?? string.Empty;
+    }
+
+    public async Task<bool> UserExistsAsync(string username)
+    {
+        var getUserByUsernameQuery = new GetUserByUsernameQuery(username);
+        var result = await userQueryService.Handle(getUserByUsernameQuery);
+        return result is not null;
+    }
+
+    public async Task<bool> UserExistsByIdAsync(int userId)
+    {
+        var getUserByIdQuery = new GetUserByIdQuery(userId);
+        var result = await userQueryService.Handle(getUserByIdQuery);
+        return result is not null;
+    }
+
+    public async Task<bool> HasActiveSubscriptionAsync(int userId)
+    {
+        var query = new GetSubscriptionByUserIdQuery(userId);
+        var subscription = await subscriptionQueryService.Handle(query);
+        return subscription is not null
+            && subscription.Status == SubscriptionStatus.Active
+            && !subscription.IsExpired();
     }
 }
