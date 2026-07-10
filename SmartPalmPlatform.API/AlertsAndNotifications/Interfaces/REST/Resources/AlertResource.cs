e02667254c739127f4efd506eaa1012aa0f57442
VRojas1603 <rojas.vmanuel16@gmail.com>
2026-07-10 07:44:11 -0500

    fix: add id to AlertResource and update assembler

--- SmartPalmPlatform.API/AlertsAndNotifications/Interfaces/REST/Resources/AlertResource.cs
+++ SmartPalmPlatform.API/AlertsAndNotifications/Interfaces/REST/Resources/AlertResource.cs
@@ -1,6 +1,7 @@
 namespace SmartPalmPlatform.API.AlertsAndNotifications.Interfaces.REST.Resources;
 
 public record AlertResource(
+    int id,
     string sensorType,
     string message,
     string level,
